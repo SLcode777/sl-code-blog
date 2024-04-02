8cb70d248b163c6b1be0ca27d035864cdd3f6e55
unknown <paperstreet.notion@gmail.com>
2024-04-02 23:14:48 +0200

    site header

---
+++ components/main-nav.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import Link from "next/link";
+import { Icons } from "./icons";
+import { siteConfig } from "@/config/site";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
+
+export function MainNav() {
+  const pathname = usePathname();
+  return (
+    <nav className="flex items-center space-x-4 lg:space-x-6">
+      <Link href="/" className="mr-6 flex items-center space-x-2">
+        <Icons.logo className="h-30 w-20" />
+        <span className="font-bold">{siteConfig.name}</span>
+      </Link>
+      <Link
+        href="/blog"
+        className={cn(
+          "text-sm font-medium transition-colors hover:text-primary",
+          pathname === "/blog" ? "text-foreground" : "text-foreground/60"
+        )}
+      >
+        Blog
+      </Link>
+      <Link
+        href="/about"
+        className={cn(
+          "text-sm font-medium transition-colors hover:text-primary",
+          pathname === "/blog" ? "text-foreground" : "text-foreground/60"
+        )}
+      >
+        About
+      </Link>
+    </nav>
+  );
+}
---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "",
+  url: "https://example.com",
+  description: "NextJS 14 blog using velite, tailwind and shadcn",
+  author: "Lucy",
+  links: {
+    github: "https://github.com/StellaDePaperStreet",
+    linkedin: "https://www.linkedin.com/in/stella-de-paperstreet-419929293/",
+    portfolio: "",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
