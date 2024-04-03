787dbc07fc304f3471e41eab3a16a85b4de50614
unknown <paperstreet.notion@gmail.com>
2024-04-03 22:22:32 +0200

    setup post page

---
+++ components/mdx-components.tsx
@@ -0,0 +1,20 @@
+import * as runtime from "react/jsx-runtime";
+import Image from "next/image";
+
+const useMDXComponent = (code: string) => {
+  const fn = new Function(code);
+  return fn({ ...runtime }).default;
+};
+
+const components = {
+  Image,
+};
+
+interface MdxProps {
+  code: string;
+}
+
+export function MDXContent({ code }: MdxProps) {
+  const Component = useMDXComponent(code);
+  return <Component components={components} />;
+}
