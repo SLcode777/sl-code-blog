5e63d25f7b576c03d6c18dc90731a3e80c7f5253
unknown <paperstreet.notion@gmail.com>
2024-04-06 10:45:47 +0200

    color changes and new article added

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -33,7 +33,7 @@ export default async function BlogPage({ searchParams }: BlogPageProps) {
         <div className="flex-1 space-y-4">
           <h1 className="inline-block font-black text-4xl lg:text-5xl">Blog</h1>
           <p className="text-xl text-muted-foreground">
-            My ramblings on all things web dev.
+            Mon parcours de reconversion en developpement Web.
           </p>
         </div>
       </div>
