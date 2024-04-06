f100a5c8dd0d832fbddf1ed18759ad3d8423443e
unknown <paperstreet.notion@gmail.com>
2024-04-06 22:31:29 +0200

    added tags filtered page

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,7 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 import { post } from "#site/content";
+import { slug } from "github-slugger";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -35,4 +36,12 @@ export function getAllTags(posts: Array<post>) {
 
 export function sortTagsByCount(tags: Record<string, number>) {
   return Object.keys(tags).sort((a, b) => tags[b] - tags[a]);
+}
+
+export function getPostsByTagSlug(posts: Array<post>, tag: string) {
+  return posts.filter((post) => {
+    if (!post.tags) return false;
+    const slugifiedTags = post.tags.map((tag) => slug(tag));
+    return slugifiedTags.includes(tag);
+  });
 }
