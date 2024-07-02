803bb42a41adce156da51986dee8ef5cd89b1d75
SLcode777 <paperstreet.notion@gmail.com>
2024-07-02 21:27:58 +0200

    fix build error by adding missing files

--- app/affiliate/affi-card.tsx
+++ app/affiliate/affi-card.tsx
@@ -2,7 +2,21 @@
 
 import Image from "next/image";
 
-export const AffiCard = ({ title , link, description, image, tag }) => {
+interface AffiCardProps {
+  title: string;
+  link: string;
+  description: string;
+  image: string;
+  tag: string;
+}
+
+export const AffiCard: React.FC<AffiCardProps> = ({
+  title,
+  link,
+  description,
+  image,
+  tag,
+}) => {
   const handleClick = () => {
     window.open(link, "_blank");
   };
@@ -36,36 +50,31 @@ export const AffiCard = ({ title , link, description, image, tag }) => {
   );
 };
 
-
 export const CodelineCard = () => {
-
   const link = "https://dub.sh/codeline-slcode";
 
   const handleClick = () => {
     window.open(link, "_blank");
   };
 
-  return(
-
-    <div className="hover:cursor-pointer"  onClick={handleClick}>
-    <h1 className="inline-block titre my-4 text-2xl lg:text-3xl">
-      La Plateforme
-    </h1>
-    <div className="border hover:border-yellow-500 rounded-lg flex flex-col items-centergap-2 gap-2 pt-2 bg-black/30 mb-8">
-      <h1 className="text-3xl font-bold text-white text-center">
-        Codeline
+  return (
+    <div className="hover:cursor-pointer" onClick={handleClick}>
+      <h1 className="inline-block titre my-4 text-2xl lg:text-3xl">
+        La Plateforme
       </h1>
-      <p className="italic text-md pl-4 text-center">
-        La meilleure plateforme de formation pour les développeurs !
-      </p>
-      <Image
-        src="/img/codeline.png"
-        alt="affi-beginjs"
-        width={850}
-        height={800}
-        className="rounded-b-lg"
-      />
+      <div className="border hover:border-yellow-500 rounded-lg flex flex-col items-centergap-2 gap-2 pt-2 bg-black/30 mb-8">
+        <h1 className="text-3xl font-bold text-white text-center">Codeline</h1>
+        <p className="italic text-md pl-4 text-center">
+          La meilleure plateforme de formation pour les développeurs !
+        </p>
+        <Image
+          src="/img/codeline.png"
+          alt="affi-beginjs"
+          width={850}
+          height={800}
+          className="rounded-b-lg"
+        />
+      </div>
     </div>
-  </div>
-
-  )}
+  );
+};
