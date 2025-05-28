146b51384d5ea0cace8dfc88e097167fd019b407
SLcode777 <paperstreet.notion@gmail.com>
2025-05-28 22:56:51 +0200

    tried to change the theme-color of the app to yellow (in the main layout)

--- app/(the-blog)/layout.tsx
+++ app/(the-blog)/layout.tsx
@@ -19,8 +19,8 @@ export const metadata: Metadata = {
 
 export const viewport: Viewport = {
   themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
+    { media: "(prefers-color-scheme: light)", color: "yellow" },
+    { media: "(prefers-color-scheme: dark)", color: "yellow" },
   ],
 };
 
